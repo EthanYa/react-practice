5b318bda95a9365d0134cc4080421e094283d454
EthanYa <green7916328@gmail.com>
2017-07-01 13:46:53 +0800

    completed RecipeFinder

--- other_practice/RecipeFinder_react-redux/components/RecipeList.js
+++ other_practice/RecipeFinder_react-redux/components/RecipeList.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import RecipeItem from './RecipeItem'
+import { Link } from 'react-router-dom'
 
 class RecipeList extends Component{
 	render(){
 		return(
 			<div>
+			<h4 className="link"><Link to='./favorites'>Favorites</Link></h4>
 			{
 				this.props.recipes.map((recipe,index)=>{
-					return(<RecipeItem key={index} recipe={recipe} />)
+					return(<RecipeItem key={index} recipe={recipe} favoriteButton={true} />)
 				})
 			}
 			</div>
--- other_practice/RecipeFinder_react-redux/index.js
+++ other_practice/RecipeFinder_react-redux/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { BrowserRouter , Switch , Route } from 'react-router-dom'
 
 import './index.css'
 import './bootstrap-3.3.7-dist/css/bootstrap.css'
 import App from './components/App'
+import FavoriteRecipeList from './components/FavoriteRecipeList'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/index'
@@ -13,5 +15,10 @@ store.subscribe(()=>console.log('store',store.getState()))
 
 ReactDom.render(
   	<Provider store={store}>
-		<App/> 
+		<BrowserRouter>
+			<Switch>
+				<Route exact path='/' component={App} />
+				<Route path='/favorites' component={FavoriteRecipeList} />
+			</Switch>
+		</BrowserRouter>
 	</Provider>, document.getElementById('root'))
