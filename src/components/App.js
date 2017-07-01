5e66fe1edc3a984ca40d6445b39dcae954645c9f
EthanYa <green7916328@gmail.com>
2017-07-01 23:16:11 +0800

    add css

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import '../index.css'
+import MemeItem from './MemeItem'
+class App extends Component {
+	constructor(){
+		super()
+
+		this.state = {
+			memeLimit: 10
+		}
+	}
+	render(){
+		return(
+			<div>
+				<h2>Welecom to the Meme Generator</h2>
+				{
+					this.props.memes.slice(0,this.state.memeLimit).map((meme,index)=>{
+						return(
+							<MemeItem key={index} meme={meme} />
+							)
+					})
+				}
+				<div className="meme-button"
+				onClick={()=>{this.setState({memeLimit:this.state.memeLimit+10})}}>
+				Load 10 more memes...</div>
+			</div>
+			)
+	}
+}
+
+function mapStateToProps(state){
+	return state
+}
+
+export default connect(mapStateToProps,null)(App)
